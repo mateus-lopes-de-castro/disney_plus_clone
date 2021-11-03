1d0f4f2a090a3fc4710ccf30df08cff053bda657
mateuslopesdecastro <mlcmateus99@gmail.com>
2021-11-03 15:24:34 -0300

    Criando estilos globais

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import GlobalStyles from './styles/GlobalStyles';
+
+const App: React.FC = () => {
+    
+    return (
+        <>
+            <GlobalStyles />
+            <span>oi</span>
+        </>
+    )
+}
+
+export default App;
