6e15d1f696624ccc61a12d3e9d5bbed26ac5aa06
mateuslopesdecastro <mlcmateus99@gmail.com>
2021-11-08 21:32:55 -0300

    Adicionando funcionalidade login na página inicial

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+import { useAuth } from '../../hooks/useAuth';
 
 import ctaLogoOne from '../../assets/cta-logo-one.svg';
 import ctaLogoTwo from '../../assets/cta-logo-two.png';
@@ -7,12 +10,23 @@ import imgBackground from '../../assets/login-background.jpg';
 import { Container, Content, BgImage, CTA, CTALogoOne, SignUp, Description, CTALogoTwo } from './styles';
 
 const Login: React.FC = () => {
+
+    const history = useHistory();
+    const { signInWithGoogle } = useAuth();
+
+    const handleSignIn = async () => {
+        await signInWithGoogle().then(res => {
+            if (res)
+                history.push('/home');
+        });
+    }
+
     return (
         <Container>
             <Content>
                 <CTA>
                     <CTALogoOne src={ctaLogoOne} alt="" />
-                    <SignUp>GET ALL THERE</SignUp>
+                    <SignUp onClick={handleSignIn}>GET ALL THERE</SignUp>
                     <Description>
                         Get Premier Access to Raya and the Last Dragon for an additional fee
                         with a Disney+ subscription. As of 03/26/21, the price of Disney+
